07e79c780833b94485848be97aae9261abc96d19
Phong Nguyen <phongnt.ptnk@gmail.com>
2018-12-31 23:23:10 +0800

    Add basic config

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+pub struct Config {
+    pub filename: String,
+    pub sum: bool
+}
+
+enum Flag {
+    Sum
+}
+
+impl Flag {
+    fn parse(arg: &str) -> Option<Flag> {
+        match arg.to_lowercase().as_ref() {
+            "-s"    => Some(Flag::Sum),
+            "--sum" => Some(Flag::Sum),
+            _       => None
+        }
+    }
+}
+
+impl Config {
+    fn set_config_flag(arg: &str, config: &mut Config) {
+        match Flag::parse(arg) {
+            Some(Flag::Sum) => config.sum = true,
+            _ => ()
+        }
+    }
+
+    pub fn new(args: std::env::Args) -> Result<Config, String> {
+        let mut filename_arg = None;
+        let mut config = Config { filename: String::from(""), sum: false };
+        for arg in args {
+            if !arg.starts_with("-") {
+                filename_arg = Some(arg);
+            } else {
+                Config::set_config_flag(&arg, &mut config);
+            }
+        }
+        match filename_arg {
+            None => {
+                Err(String::from("no filename provided"))
+            }
+            Some(s) => {
+                config.filename = s;
+                Ok(config)
+            }
+        }
+    }
+}
+
+pub struct Accumulator<T> {
+    pub sum: T
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate st;
+use st::*;
+use std::env;
+
+fn read_file(filename: &str) {
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::prelude::*;
+
+    let file = File::open(filename).unwrap();
+    for line in BufReader::new(file).lines() {
+        println!("{}", line.unwrap());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args();
+    let config = st::Config::new(args).unwrap();
+    println!("Config: {}, sum = {}", config.filename, config.sum);
+    read_file(&config.filename);
 }
